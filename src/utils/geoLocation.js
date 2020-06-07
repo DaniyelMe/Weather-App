766ba804cd1716b2ef2c91f4304b15597342a311
Daniyel <DaniyelMenda@Gmail.com>
2020-06-07 23:43:14 +0300

    feat: Adds AccuWeather Locations and Forecast APIs. Web APIs Geolocation

---
+++ src/utils/geoLocation.js
@@ -0,0 +1,70 @@
+const apiKey = 'YFWtanEKLIJf8jbhQFxpJ1DInelAzxVY';
+
+/* Locations API */
+
+// Geolocation API - Web APIs
+const getCurrentPosition = async function() {
+	if (!navigator.geolocation) {
+		return 'Geolocation is not supported by your browser';
+	} else {
+		navigator.geolocation.getCurrentPosition(
+			position => {
+				return {
+					lot: position.coords.latitude,
+					long: position.coords.longitude
+				};
+			},
+			error => {
+				return 'Unable to retrieve your location';
+			}
+		);
+	}
+	return 'Unable to retrieve your location';
+};
+
+// Returns information about a specific location, by GeoPosition (Latitude and Longitude).
+const geoPosition = async function(lat, long) {
+	const url = 'https://dataservice.accuweather.com/locations/v1/cities/geoposition/search';
+	const params = `?apikey=${apiKey}&q=${lat},${long}&language=en-us HTTP/1.1`;
+
+	return await fetch(`${url}${params}`)
+		.then(res => res.json())
+		.then(out => {
+			return out;
+		});
+};
+
+// Returns basic information about locations matching an autocomplete of the search text.
+const searchAutoComplete = async function(inputVal) {
+	const url = 'https://dataservice.accuweather.com/locations/v1/cities/autocomplete';
+	const params = `?apikey=${apiKey}&q=${inputVal}&language=en-us HTTP/1.1`;
+
+	return await fetch(`${url}${params}`)
+		.then(res => res.json())
+		.then(out => {
+			console.log('out', out);
+			return out;
+		});
+};
+
+/* Forecast API */
+
+// Returns an array of daily forecasts for the next 5 days for a specific location.
+const fiveDaysForecast = async function(inputVal) {
+	const url = 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/';
+	const params = `${inputVal}?apikey=${apiKey}&language=en-us&metric=true HTTP/1.1`;
+
+	return await fetch(`${url}${params}`)
+		.then(res => res.json())
+		.then(out => {
+			console.log('out', out);
+			return out;
+		});
+};
+
+export default {
+	getCurrentPosition,
+	geoPosition,
+	searchAutoComplete,
+	fiveDaysForecast
+};
