8480001a453eba75630462dc9caf07ca94ca5e86
Daniyel <DaniyelMenda@Gmail.com>
2020-06-07 10:23:00 +0300

    refactor: Spacing, Renaming at Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,63 +1,55 @@
 <template>
-  <div class="home">
+	<div class="home">
 		<SiteHeader></SiteHeader>
-    <div class="search-bar">
-      <span>ICON</span>
-      <span>Search Bar</span>
-      <input type="text" />
-    </div>
 
-    <main class="home-main">
-      <section class="home-main-top">
-        <span class="location-card">location</span>
+		<div class="search-bar">
+			<span>ICON</span>
+			<span>Search Bar</span>
+			<input type="text" />
+		</div>
 
-        <span class="location-card">degree</span>
+		<main class="home-main">
+			<section class="home-main-top">
+				<span class="location-card">Location</span>
 
-        <button>Add to Favorites</button>
-      </section>
+				<span class="location-card">Degree</span>
 
-      <section class="location-cards">
-        <span v-for="(day) in nextFiveDays" :key="day" class="location-card">{{day}}</span>
-      </section>
-    </main>
+				<button>Add to Favorites</button>
+			</section>
 
-    <footer></footer>
-  </div>
+			<section class="location-cards">
+				<span v-for="day in nextFiveDays" :key="day" class="location-card">{{ day }}</span>
+			</section>
+		</main>
+
+		<footer>Made with ❤️ 2020</footer>
+	</div>
 </template>
 
 <script>
 import SiteHeader from './SiteHeader';
+
 export default {
 	components: { SiteHeader },
-  data() {
-    return {
-      daysOfWeek: [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-      ]
-    };
-  },
-  computed: {
-    nextFiveDays: function() {
-      const todayIndex = new Date().getDay();
-      let fiveDays = [];
+	data() {
+		return {
+			daysOfWeek: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+		};
+	},
+	computed: {
+		nextFiveDays: function() {
+			const todayIndex = new Date().getDay();
+			let fiveDays = [];
 
-      // From today till end of the week.
-      for (let i = todayIndex; i < this.daysOfWeek.length; i++)
-        fiveDays.push(this.daysOfWeek[i]);
+			// From today till end of the week.
+			for (let i = todayIndex; i < this.daysOfWeek.length < 5; i++) fiveDays.push(this.daysOfWeek[i]);
 
-      // From the start of the week till we have 5 days.
-      for (let i = 0; fiveDays.length < 5; i++)
-        fiveDays.push(this.daysOfWeek[i]);
+			// From the start of the week till we have 5 days.
+			for (let i = 0; fiveDays.length < 5; i++) fiveDays.push(this.daysOfWeek[i]);
 
-      return fiveDays;
-    }
-  }
+			return fiveDays;
+		}
+	}
 };
 </script>
 
