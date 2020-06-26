c938164d5d769cee8a5b8934c34f0281e2ef7b40
Daniyel <DaniyelMenda@Gmail.com>
2020-06-26 18:05:09 +0300

    refactor: Adds comments to icons, update License

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,5 +1,6 @@
 const getIcons = function(id) {
 	const origin = 'https://developer.accuweather.com/sites/default/files/';
+	//Adds zero before numbers from 1-9
 	const number = id < 10 ? `0${id}` : id;
 
 	return origin + number + '-s.png';
