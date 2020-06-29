cc7cbb0088fc09e30aba0c2f75cef03ce3fce738
Daniyel <DaniyelMenda@Gmail.com>
2020-06-29 20:21:35 +0300

    refactor: Loader component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,8 @@
-<template>
+const Loading = () => {
 	<div class="loader">
 		<div class="one"></div>
 		<div class="two"></div>
-	</div>
-</template>
+	</div>;
+};
 
-<script>
-export default {};
-</script>
-
+export default Loading;
