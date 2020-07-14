2e5fcb8e1168704bd96d06551f29a29cfcb3db5f
Daniyel <DaniyelMenda@Gmail.com>
2020-07-14 21:08:25 +0300

    refactor: SiteFooter

--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -1,27 +1,43 @@
 import React from 'react';
-const SiteFooter = ({ theme, setTheme }, { metric, setMetric }) => {
-	const toggleTheme = () => {
-		setTheme = !theme;
-	};
-	const toggleMetric = () => {
-		setMetric = !metric;
-	};
+
+const SiteFooter = ({ handler, theme, metric }) => {
+	function toggleTheme() {
+		handler('setTheme', !theme);
+	}
+	function toggleMetric() {
+		handler('setMetric', !metric);
+	}
+
+	document.body.setAttribute('class', theme ? 'light-mode' : 'dark-mode');
 
 	return (
-		<footer class="site-footer">
-			<div class="toggle toggle--daynight">
-				<input type="checkbox" checked={theme} id="toggle--daynight" class="toggle--checkbox" onClick="toggleTheme" />
-				<label class="toggle--btn" for="toggle--daynight">
-					<span class="toggle--feature"></span>
+		<footer className="site-footer">
+			<div className="toggle toggle--daynight">
+				<input
+					type="checkbox"
+					checked={theme}
+					id="toggle--daynight"
+					className="toggle--checkbox"
+					onChange={toggleTheme}
+				/>
+				<label className="toggle--btn" htmlFor="toggle--daynight">
+					<span className="toggle--feature"></span>
 				</label>
 			</div>
 
 			<span>Made with ❤️ 2020</span>
 
-			<div class="toggle toggle--degree">
-				<input type="checkbox" checked={metric} id="toggle--degree" class="toggle--checkbox" onClick="toggleMetric" />
-				<label class="toggle--btn" for="toggle--degree">
-					<span class="toggle--feature"></span>
+			<div className="toggle toggle--degree">
+				<input
+					type="checkbox"
+					checked={metric}
+					htmlFor="toggle--degree"
+					id="toggle--degree"
+					className="toggle--checkbox"
+					onChange={toggleMetric}
+				/>
+				<label className="toggle--btn" htmlFor="toggle--degree">
+					<span className="toggle--feature"></span>
 				</label>
 			</div>
 		</footer>
