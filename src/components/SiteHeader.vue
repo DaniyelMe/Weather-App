0883fc9305b7ea89d443897c28df45fbdd889a80
Daniyel <DaniyelMenda@Gmail.com>
2020-08-17 23:47:45 +0300

    refactor: SiteHeader

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,48 +1,30 @@
 <template>
 	<header class="site-header nonselected">
 		<div class="site-header-nav">
-			<router-link :to="{ name: 'home' }" class="logo nonselected">
-				<img src="../assets/logo.svg" alt="weather app logo" />
-			</router-link>
-
-			<ol>
-				<li>
-					<router-link :to="{ name: 'home' }" :class="{ 'active-path': path == 'home' }">Home</router-link>
-				</li>
-
-				<li>
-					<router-link :to="{ name: 'favorites' }" :class="{ 'active-path': path == 'favorites' }"
-						>Favorites</router-link
-					>
-				</li>
-			</ol>
+			<span class="material-icons">place</span>
+			<h4>{{ location.name }}</h4>
 		</div>
 
-		<div v-if="!isFind" class="add-city">
-			<button class="add-location nonselected button-hover-active" @click="isFindCity">
-				<div class="add-new-location-text">Add Location</div>
-				<span class="material-icons">add</span>
-			</button>
+		<div class="header-menu">
+			<span class="material-icons" @click="openMenu = !openMenu">menu</span>
+			<ol v-if="openMenu">
+				<li v-for="item in menu" :key="item">{{ item }}</li>
+			</ol>
 		</div>
 	</header>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-
 export default {
-	computed: {
-		isFind() {
-			return this.$store.state.app.isFindCity;
-		},
-
-		path() {
-			return this.$route.name;
-		}
+	data() {
+		return {
+			openMenu: false,
+			menu: ['metric', 'theme']
+		};
 	},
-	methods: {
-		isFindCity() {
-			this.$store.commit('toggleFind', true);
+	computed: {
+		location() {
+			return this.$store.state.app.currentPosition;
 		}
 	}
 };
